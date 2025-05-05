191e1b87ad47973040ebb3c8a79908e019fae5f9
G. Aslan <13198950+Gaslan@users.noreply.github.com>
2025-05-06 00:14:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
-const roadTexture = new Image();
 const carImagePath = './assets/tdrc01_car09_f.png'
 const roadTexturePath = './assets/asphalt_road_2.jpg'
 
@@ -9,12 +8,17 @@ const Canvas = {
   dimensions: {
     width: canvas.width,
     height: canvas.height
+  },
+  resize() {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    this.dimensions.width = canvas.width
+    this.dimensions.height = canvas.height
   }
 }
 
 const Car = {
   image: new Image(),
-  imagePath: carImagePath,
   coords: {
     x: 50,
     y: 100
@@ -22,36 +26,52 @@ const Car = {
   dimensions: {
     width: 28.3,
     height: 59.4
+  },
+  init() {
+    const _this = this
+    this.image.src = carImagePath
+    this.image.onload = function () {
+      _this.draw()
+    }
+    this.coords.x = Canvas.dimensions.width / 2 - this.dimensions.width / 2
+  },
+  draw() {
+    const x = this.coords.x < 0 ? 0 : this.coords.x + this.dimensions.width > Canvas.dimensions.width ? Canvas.dimensions.width - this.dimensions.width : this.coords.x
+    const y = Canvas.dimensions.height - this.dimensions.height - 40
+    ctx.drawImage(this.image, x, y, this.dimensions.width, this.dimensions.height)
   }
 }
 
-const textureWidth = 256
-const textureHeight = 256
+const Road = {
+  image: new Image(),
+  dimensions: {
+    width: 256,
+    height: 256
+  },
+  init() {
+    const _this = this
+    this.image.src = roadTexturePath
+    this.image.onload = function () {
+      _this.draw()
+    }
+  },
+  draw() {
+    const x = (canvas.width / 2) - (Road.dimensions.width / 2)
+    for (let y = -Road.dimensions.height; y < canvas.height; y += Road.dimensions.height) {
+      ctx.drawImage(this.image, x, y + offsetY, Road.dimensions.width, Road.dimensions.height)
+    }
+  }
+}
 
 let lastTouchX = null
 let offsetY = 0
 const speed = 1
 
-
-
-function resizeCanvas() {
-  canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
-  Canvas.dimensions.width = canvas.width
-  Canvas.dimensions.height = canvas.height
-  Car.coords.x = Canvas.dimensions.width / 2 - Car.dimensions.width / 2
-}
-
 canvas.addEventListener('touchmove', function (e) {
   e.preventDefault()
   const touch = e.touches[0]
-
-  const x = touch.clientX
-  const dx = x - lastTouchX
-
-  Car.coords.x += dx
-  lastTouchX = x
-
+  Car.coords.x += touch.clientX - lastTouchX
+  lastTouchX = touch.clientX
   requestAnimationFrame(drawFrame)
 }, { passive: false })
 
@@ -59,22 +79,13 @@ canvas.addEventListener('touchstart', function (e) {
   lastTouchX = e.touches[0].clientX
 })
 
-
 window.addEventListener('load', function () {
-  resizeCanvas()
-
-  roadTexture.src = roadTexturePath
-  roadTexture.onload = function () {
-    drawRoad()
-  }
-
-  Car.image.src = Car.imagePath
-  Car.image.onload = function () {
-    drawCar()
-  }
+  Canvas.resize()
+  Road.init()
+  Car.init()
 
   const interval = setInterval(function() {
-    offsetY = offsetY >= textureHeight ? 0 : offsetY + speed
+    offsetY = offsetY >= Road.dimensions.height ? 0 : offsetY + speed
     requestAnimationFrame(drawFrame)
   }, 15)
 
@@ -83,21 +94,8 @@ window.addEventListener('load', function () {
   })
 })
 
-function drawCar() {
-  const x = Car.coords.x < 0 ? 0 : Car.coords.x + Car.dimensions.width > Canvas.dimensions.width ? Canvas.dimensions.width - Car.dimensions.width : Car.coords.x
-  const y = Canvas.dimensions.height - Car.dimensions.height - 40
-  ctx.drawImage(Car.image, x, y, Car.dimensions.width, Car.dimensions.height)
-}
-
-function drawRoad() {
-  const x = (canvas.width / 2) - (textureWidth / 2)
-  for (let y = -textureHeight; y < canvas.height; y += textureHeight) {
-    ctx.drawImage(roadTexture, x, y + offsetY, textureWidth, textureHeight)
-  }
-}
-
 function drawFrame() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-  drawRoad()
-  drawCar()
+  Road.draw()
+  Car.draw()
 }
