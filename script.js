7c4852421d223ab981fe54d8560b86ce2df50f8f
G. Aslan <13198950+Gaslan@users.noreply.github.com>
2025-05-06 22:53:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
-const carImagePath = './assets/tdrc01_car09_f.png'
+const carImagePath = './assets/Car.png'
 const roadTexturePath = './assets/asphalt_road_2.jpg'
+
+const car1Path = './assets/Car1.png'
+const car2Path = './assets/Car2.png'
+const car3Path = './assets/Car3.png'
+const car4Path = './assets/Car4.png'
+
+const onCrushed = new CustomEvent('crushed')
+
+const rivalCarPaths = [
+  './assets/Car11.png',
+  './assets/Car12.png',
+  './assets/Car13.png',
+  './assets/Car14.png',
+  './assets/Car15.png'
+]
+
+function randomNumber(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 const Canvas = {
   dimensions: {
@@ -34,11 +53,11 @@ const Car = {
       _this.draw()
     }
     this.coords.x = Canvas.dimensions.width / 2 - this.dimensions.width / 2
+    this.coords.y = Canvas.dimensions.height - this.dimensions.height - 40
   },
   draw() {
     const x = this.coords.x < 0 ? 0 : this.coords.x + this.dimensions.width > Canvas.dimensions.width ? Canvas.dimensions.width - this.dimensions.width : this.coords.x
-    const y = Canvas.dimensions.height - this.dimensions.height - 40
-    ctx.drawImage(this.image, x, y, this.dimensions.width, this.dimensions.height)
+    ctx.drawImage(this.image, x, this.coords.y, this.dimensions.width, this.dimensions.height)
   }
 }
 
@@ -63,16 +82,102 @@ const Road = {
   }
 }
 
+function RivalCar(speed) {
+  const _this = this
+  this.speed = speed
+  this.position = {
+    x: Canvas.dimensions.width * Math.random(),
+    y: 0
+  }
+  this.dimensions = {
+    width: 28.3,
+    height: 59.4
+  }
+  this.image = new Image()
+  this.image.src = rivalCarPaths[randomNumber(0, 4)]
+  this.image.onload = function () {
+    _this.draw()
+  }
+  this.destroy = function() {}
+}
+
+RivalCar.prototype.draw = function draw() {
+  ctx.drawImage(this.image, this.position.x, this.position.y, this.dimensions.width, this.dimensions.height)
+}
+
+RivalCar.prototype.move = function move() {
+  if (this.position.y > Canvas.dimensions.height) {
+    this.destroy()
+  }
+  this.position.y += this.speed * 0.1
+}
+
+RivalCar.prototype.setDestroy = function setDestroy(fn) {
+  this.destroy = fn
+}
+
+const RivalCarManager = {
+  cars: [],
+  distance: 0,
+  init() {
+    const _this = this
+    const interval = setInterval(function() {
+      const rand = Math.random()
+      if (rand > 0.8) {
+        _this.createRivalCar()
+      }
+    }, 200)
+    document.addEventListener('crushed', function() {
+      clearInterval(interval)
+    })
+  },
+  createRivalCar() {
+    const _this = this
+    const newCar = new RivalCar(45)
+    newCar.setDestroy(function() {
+      _this.cars = _this.cars.filter(car => car != this)
+    })
+    this.cars.push(newCar)
+  },
+  checkCrush() {
+    const crushed = this.cars.some(car => {
+      if (car.position.y + car.dimensions.height < Car.coords.y) {
+        return false
+      }
+
+      if (car.position.y > Car.coords.y + Car.dimensions.height) {
+        return false
+      }
+
+      if (car.position.x + car.dimensions.width > Car.coords.x && car.position.x + car.dimensions.width < Car.coords.x + Car.dimensions.width) {
+        return true
+      }
+
+      if (car.position.x < Car.coords.x + Car.dimensions.width && car.position.x > Car.coords.x) {
+        return true
+      }
+
+      return false
+    })
+    if (crushed) {
+      document.dispatchEvent(onCrushed)
+    }
+  },
+  move(v) {
+    this.distance += v
+    document.getElementById('score').innerHTML = this.distance + ' meters'
+  }
+}
+
 let lastTouchX = null
 let offsetY = 0
-const speed = 1
+const speed = 8
 
 canvas.addEventListener('touchmove', function (e) {
   e.preventDefault()
   const touch = e.touches[0]
   Car.coords.x += touch.clientX - lastTouchX
   lastTouchX = touch.clientX
-  requestAnimationFrame(drawFrame)
 }, { passive: false })
 
 canvas.addEventListener('touchstart', function (e) {
@@ -83,13 +188,19 @@ window.addEventListener('load', function () {
   Canvas.init()
   Road.init()
   Car.init()
+  RivalCarManager.init()
 
-  const interval = setInterval(function() {
+  const interval = setInterval(function () {
     offsetY = offsetY >= Road.dimensions.height ? 0 : offsetY + speed
+    RivalCarManager.cars.forEach(car => car.move())
     requestAnimationFrame(drawFrame)
-  }, 15)
+  }, 10)
 
   window.addEventListener('unload', function () {
+    clearInterval(interval)
+  })
+
+  this.document.addEventListener('crushed', function() {
     clearInterval(interval)
   })
 })
@@ -98,4 +209,7 @@ function drawFrame() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   Road.draw()
   Car.draw()
+  RivalCarManager.cars.forEach(car => car.draw())
+  RivalCarManager.checkCrush()
+  RivalCarManager.move(1)
 }
